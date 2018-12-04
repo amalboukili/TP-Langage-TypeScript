d726dc10f635ec98d824b1f3082c446e42973254
Amal Boukili <boukili.amal@gmail.com>
2018-12-04 22:42:16 +0100

    POO => Héritage

--- classes.ts
+++ classes.ts
@@ -11,3 +11,17 @@ class User{
 }
 let user=new User('Jean', 35);
 user.affiche();
+
+// Héritage
+class Redactor extends User{
+    public category:string;
+    constructor(name:string,age:number,category:string){
+        super(name,age);
+        this.category=category;
+    }
+    affiche(){
+        console.log('Bonjour '+this.name+' de la catégorie '+this.category+' !');
+    }
+}
+let redactor=new Redactor('Jean',35,'Jeunesse');
+redactor.affiche();
